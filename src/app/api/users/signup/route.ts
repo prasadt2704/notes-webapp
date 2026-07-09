4584b9fa05af255bb068eb9e84c21752428fb4cd
jtanpup_jcplc <jtanpup@jci.com>
2026-07-09 11:21:00 +0530

    removed mail changes

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,7 +2,6 @@ import { connect } from "../../../../dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import { sendEmail } from "@/helpers/mailer";
 
 connect();
 export async function POST(req: NextRequest) {
@@ -19,13 +18,9 @@ export async function POST(req: NextRequest) {
     const salt = await bcryptjs.genSalt(10);
     const hashedPassword = await bcryptjs.hash(password, salt);
 
-
-
     //Create a new user
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
-
-    await sendEmail({ email, userId: String(newUser._id), emailType: "VERIFY" });
 
     return NextResponse.json({ message: "User created successfully" }, { status: 201 });
   } catch (error) {
